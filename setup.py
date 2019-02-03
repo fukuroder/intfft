cfdb1c4d884ffe2c549e18ed0a406b75a3659809
fukuroda <fukuroder@live.jp>
2019-02-03 20:17:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup (name = 'pyintfft',
+setup (name = 'intfft',
        version = '0.1.0',
        description = 'Integer FFT(Fast Fourier Transform) in Python',
        install_requires = ['numpy'],
